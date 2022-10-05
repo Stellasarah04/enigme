fa5e75d4abd7e98cdcfc9648894495d23f163a77
Stellasarah04 <sarah.auzet@ynov.com>
2022-10-05 21:19:06 +0100

    Projet Hangman générer un nombre

--- Projettxt.go
+++ Projettxt.go
@@ -21,18 +21,28 @@ func main() {
 
 func ReadFile(Mot string) []string {
 
-	var Tableau = []string{"Hangman.txt"}
+	var Tableau = []string{"generate", "string", "number", "make", "find", "grow", "array", "node", "chicago", "list", "do", "type", "78", "before", "23", "structure", "int", "drink", "7", "set", "car", "get", "random", "are", "now", "number", "mod", "after", "file", "ynov", "print", "keep", "windows", "show", "read", "86", "hangman", "a"}
+
+	fmt.Println(Tableau[0])
+
+	fmt.Println(Tableau[len(Tableau)-1])
 
 	for _, i := range Tableau {
-		if i == "\n" {
-			Tableau = append(Tableau, Mot)
-			Mot = ""
-		} else {
-			Mot += string(i)
-		}
-	}
 
-	fmt.Println(Tableau)
+		if i == "before" {
+
+			fmt.Println(Tableau[14:15])
+
+			if Tableau[23] == "are" {
+
+				fmt.Println((97 + 114 + 101) / 114)
+				fmt.Println(Tableau[2])
+
+			}
+
+		}
+
+	}
 
 	return Tableau
 
